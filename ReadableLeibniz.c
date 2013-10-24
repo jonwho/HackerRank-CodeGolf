8ac3064ba7a46a6d8bd421553a63090350d84fe3
Jonathan Ho <LJ080805@gmail.com>
2013-10-23 17:57:23 -0700

    First commit.

---
+++ ReadableLeibniz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+  int T,n;
+  double s;
+
+  scanf("%d", &T);
+
+  while(T--)
+  {
+    scanf("%d", &n);
+
+    while(n--)
+      s+=(n % 2 ? -1.0 : 1.0) / ( 2 * n + 1);
+
+    printf("%.15lf\n", s);
+    s=0;
+  }
+}
